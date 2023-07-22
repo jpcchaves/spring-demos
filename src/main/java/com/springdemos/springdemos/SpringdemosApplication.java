1d54d9bb6b65a4e978bcde6aad74f450143f7baa
João Paulo <jpcchaves@outlook.com>
2023-07-22 10:00:35 -0300

    first commit

---
+++ src/main/java/com/springdemos/springdemos/SpringdemosApplication.java
@@ -0,0 +1,13 @@
+package com.springdemos.springdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemosApplication.class, args);
+	}
+
+}
