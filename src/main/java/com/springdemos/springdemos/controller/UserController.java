6cbcfb98ca23e5ed94233102cc1a3ecb2d51024d
João Chaves <joao.chaves@liferay.com>
2024-01-12 21:35:59 -0300

    feat: add user controller with list and create endpoints

---
+++ src/main/java/com/springdemos/springdemos/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.springdemos.springdemos.controller;
+
+import com.springdemos.springdemos.data.dto.UserCreateDto;
+import com.springdemos.springdemos.entity.User;
+import com.springdemos.springdemos.service.user.contracts.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        return ResponseEntity.ok(userService.listUser());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @ModelAttribute UserCreateDto requestDto) throws IOException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(requestDto));
+    }
+}
